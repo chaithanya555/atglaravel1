7c92d4b404af488a5e9fd24c862f7dd1c18028d2
chaithanya555 <39940692+chaithanya555@users.noreply.github.com>
2021-03-23 22:40:35 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,15 @@
 					</div>
 				@endif
 				</div>
-                
+                 @if ($errors->any())
+                    <div class="alert alert-danger">
+                    <ul>
+                 @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                 @endforeach
+                    </ul>
+                </div>
+                @endif
 				<form action="{{route('store')}}" method="post">
 					@csrf
                 <div class="form-content">
